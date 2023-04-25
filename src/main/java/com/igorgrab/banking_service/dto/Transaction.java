28d37ba52bc3eff92fdbed76b483feb0908c047a
[Grab I.] <grabigor1990@gmail.com>
2023-04-25 11:38:46 +0200

    added Transaction

--- src/main/java/com/igorgrab/banking_service/dto/Transaction.java
+++ src/main/java/com/igorgrab/banking_service/dto/Transaction.java
@@ -1,2 +1,14 @@
-package com.igorgrab.banking_service.dto;public class Transaction {
+package com.igorgrab.banking_service.dto;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+public class Transaction {
+
+    private Long id;
+    private BigDecimal amount;
+    private BankAccount bankAccount;
+    private String referenceNumber;
 }
