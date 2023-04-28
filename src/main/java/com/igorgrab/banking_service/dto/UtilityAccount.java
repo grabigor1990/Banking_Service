0daa0ff3644b4aa3dbf9b76287ceab0b10c2f67c
[Grab I.] <grabigor1990@gmail.com>
2023-04-28 15:10:43 +0200

    added UtilityAccount to dto

--- src/main/java/com/igorgrab/banking_service/dto/UtilityAccount.java
+++ src/main/java/com/igorgrab/banking_service/dto/UtilityAccount.java
@@ -1,2 +1,11 @@
-package com.igorgrab.banking_service.dto;public class UtilityAccount {
+package com.igorgrab.banking_service.dto;
+
+import lombok.Data;
+
+@Data
+public class UtilityAccount {
+
+    private Long id;
+    private String number;
+    private String providerNumber;
 }
