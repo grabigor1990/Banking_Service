aa841e803b56e96693b1ad4c125438699fc58807
[Grab I.] <grabigor1990@gmail.com>
2023-04-29 13:36:45 +0200

    added repository

--- src/main/java/com/igorgrab/banking_service/repository/UserRepository.java
+++ src/main/java/com/igorgrab/banking_service/repository/UserRepository.java
@@ -1,2 +1,12 @@
-package com.igorgrab.banking_service.repository;public interface UserRepository {
+package com.igorgrab.banking_service.repository;
+
+import com.igorgrab.banking_service.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<UserEntity,Long> {
+
+    Optional<UserEntity> findByIdentificationNumber(String identificationNumber);
+
 }
--- src/main/java/com/igorgrab/banking_service/repository/UtilityAccountRepository.java
+++ src/main/java/com/igorgrab/banking_service/repository/UtilityAccountRepository.java
@@ -1,2 +1,11 @@
-package com.igorgrab.banking_service.repository;public interface UtilityAccountRepository {
+package com.igorgrab.banking_service.repository;
+
+import com.igorgrab.banking_service.entity.UtilityAccountEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UtilityAccountRepository extends JpaRepository<UtilityAccountEntity,Long> {
+
+    Optional<UtilityAccountEntity> findByProviderName(String providerName);
 }
